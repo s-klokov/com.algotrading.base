46b9cc43d3b5b357fd371c5d1b7a0e25aece28d8
s-klokov <s-klokov@yandex.ru>
2021-03-22 11:09:25 +0600

    Structure commit.

---
+++ src/module-info.java
@@ -0,0 +1,27 @@
+module com.algotrading.base {
+    requires java.mail;
+    requires java.logging;
+    requires json.simple;
+    requires activation;
+    exports com.algotrading.base.core;
+    exports com.algotrading.base.core.candles;
+    exports com.algotrading.base.core.columns;
+    exports com.algotrading.base.core.commission;
+    exports com.algotrading.base.core.csv;
+    exports com.algotrading.base.core.indicators;
+    exports com.algotrading.base.core.level2;
+    exports com.algotrading.base.core.marketdata;
+    exports com.algotrading.base.core.series;
+    exports com.algotrading.base.core.sync;
+    exports com.algotrading.base.core.tester;
+    exports com.algotrading.base.core.values;
+    exports com.algotrading.base.core.window;
+    exports com.algotrading.base.helpers;
+    exports com.algotrading.base.lib;
+    exports com.algotrading.base.lib.fits;
+    exports com.algotrading.base.lib.linear;
+    exports com.algotrading.base.lib.nnets;
+    exports com.algotrading.base.lib.optim;
+    exports com.algotrading.base.lib.stat;
+    exports com.algotrading.base.util;
+}
