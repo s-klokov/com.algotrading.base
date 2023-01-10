57effd6f79ab2f77f24a473115da00b78e391bc1
s-klokov <s-klokov@yandex.ru>
2023-01-10 17:03:49 +0600

    [*] TimeFilters.java: метод для создания произвольных фильтров; рефакторинг предопределённых фильтров.

--- src/com/algotrading/base/core/TimeFilters.java
+++ src/com/algotrading/base/core/TimeFilters.java
@@ -8,48 +8,52 @@ import java.util.function.LongPredicate;
 public class TimeFilters {
 
     private TimeFilters() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Получить фильтр по времени между указанными границами.
+     *
+     * @param hhmmFrom начало
+     * @param hhmmTill конец
+     * @return фильтр
+     */
+    public static LongPredicate between(final int hhmmFrom, final int hhmmTill) {
+        if (hhmmFrom <= hhmmTill) {
+            return t -> {
+                final int hhmm = TimeCodes.hhmm(t);
+                return hhmmFrom <= hhmm && hhmm < hhmmTill;
+            };
+        } else {
+            return t -> {
+                final int hhmm = TimeCodes.hhmm(t);
+                return hhmm >= hhmmFrom || hhmm < hhmmTill;
+            };
+        }
     }
 
     /**
      * Фильтр для включения утренней сессии и дневной сессии и исключения вечерней сессии.
      */
-    public static final LongPredicate FILTER_0900_1850 = t -> {
-        final int hhmm = TimeCodes.hhmm(t);
-        return 900 <= hhmm && hhmm < 1850;
-    };
+    public static final LongPredicate FILTER_0900_1850 = between(900, 1850);
     /**
      * Фильтр для акций: с 10:00 до 18:40 (дневная сессия без аукционов открытия и закрытия).
      */
-    public static final LongPredicate FILTER_1000_1840 = t -> {
-        final int hhmm = TimeCodes.hhmm(t);
-        return 1000 <= hhmm && hhmm < 1840;
-    };
+    public static final LongPredicate FILTER_1000_1840 = between(1000, 1840);
     /**
      * Фильтр для фьючерсов: с 10:00 до 18:45 (только дневная сессия).
      */
-    public static final LongPredicate FILTER_1000_1845 = t -> {
-        final int hhmm = TimeCodes.hhmm(t);
-        return 1000 <= hhmm && hhmm < 1845;
-    };
+    public static final LongPredicate FILTER_1000_1845 = between(1000, 1845);
     /**
      * Фильтр для фьючерсов: с 10:00 до 18:50 (только дневная сессия).
      */
-    public static final LongPredicate FILTER_1000_1850 = t -> {
-        final int hhmm = TimeCodes.hhmm(t);
-        return 1000 <= hhmm && hhmm < 1850;
-    };
+    public static final LongPredicate FILTER_1000_1850 = between(1000, 1850);
     /**
      * Фильтр для фьючерсов: с 10:00 до 23:50 (дневная и вечерняя сессии).
      */
-    public static final LongPredicate FILTER_1000_2350 = t -> {
-        final int hhmm = TimeCodes.hhmm(t);
-        return 1000 <= hhmm && hhmm < 2350;
-    };
+    public static final LongPredicate FILTER_1000_2350 = between(1000, 2350);
     /**
      * Фильтр для американских акций: с 9:30 до 16:00.
      */
-    public static final LongPredicate FILTER_0930_1600 = t -> {
-        final int hhmm = TimeCodes.hhmm(t);
-        return 930 <= hhmm && hhmm < 1600;
-    };
+    public static final LongPredicate FILTER_0930_1600 = between(930, 1600);
 }
