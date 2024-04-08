31b66f2ca6f98ebd876c9752901cf4b64255d34a
s-klokov <s-klokov@yandex.ru>
2024-04-08 13:18:17 +0600

    [*] IOHelper.java: удаление методов getResponse и readNonEmptyTrimmedLines.

--- src/com/algotrading/base/helpers/IOHelper.java
+++ src/com/algotrading/base/helpers/IOHelper.java
@@ -1,13 +1,7 @@
 package com.algotrading.base.helpers;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -48,74 +42,5 @@ public class IOHelper {
      */
     public static PrintStream getPrintStream(final String fileName) throws IOException {
         return getPrintStream(new File(fileName));
-    }
-
-    /**
-     * Прочитать из файла все непустые строки и применить к каждой из них метод trim.
-     *
-     * @param fileName имя файла
-     * @return список строк
-     * @throws IOException если произошла ошибка ввода-вывода
-     */
-    public static List<String> readNonEmptyTrimmedLines(final String fileName) throws IOException {
-        return readNonEmptyTrimmedLines(new File(fileName));
-    }
-
-    /**
-     * Прочитать из файла все непустые строки и применить к каждой из них метод trim.
-     *
-     * @param file файл
-     * @return список строк
-     * @throws IOException если произошла ошибка ввода-вывода
-     */
-    public static List<String> readNonEmptyTrimmedLines(final File file) throws IOException {
-        final List<String> lines = new ArrayList<>();
-        try (final BufferedReader br = Files.newBufferedReader(file.toPath())) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                if (!line.isEmpty()) {
-                    lines.add(line);
-                }
-            }
-        }
-        return lines;
-    }
-
-    /**
-     * Получить ответ от web-сервера на запрос GET.
-     *
-     * @param request запрос к web-серверу
-     * @param timeout таймаут в миллисекундах
-     * @return список полученных строк (пустой в случае ошибок)
-     */
-    public static List<String> getResponse(final String request, final int timeout) {
-        final List<String> lines = new ArrayList<>();
-        try {
-            final URI uri;
-            try {
-                uri = new URI(request);
-            } catch (final URISyntaxException e) {
-                throw new IOException("Incorrect request string", e);
-            }
-            final HttpURLConnection con = (HttpURLConnection) uri.toURL().openConnection();
-            con.setConnectTimeout(timeout);
-            con.setReadTimeout(timeout);
-            con.connect();
-            final int code = con.getResponseCode();
-            if (code >= 400) {
-                return lines;
-            }
-            try (final BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8), 65536)) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    lines.add(line);
-                }
-            }
-            return lines;
-        } catch (final IOException e) {
-            lines.clear();
-            return lines;
-        }
     }
 }
